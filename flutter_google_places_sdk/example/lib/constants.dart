cc2abcea09a54dd04712fb3ee08f566307f25c2b
Matan Shukry <matanshukry@gmail.com>
2024-08-17 16:27:20 +0300

    key from env

--- flutter_google_places_sdk/example/lib/constants.dart
+++ flutter_google_places_sdk/example/lib/constants.dart
@@ -2,9 +2,9 @@ library constants;
 
 import 'package:flutter/material.dart';
 
-/// note: do NOT store your api key in here or in the code at all.
-/// use an external source such as file or firebase remote config
-const INITIAL_API_KEY = '';
+/// Define your api in run args, such as
+/// --dart-define API_KEY="<api_key>"
+const INITIAL_API_KEY = String.fromEnvironment('API_KEY');
 
 /// Initial value that is used for the locale
 const INITIAL_LOCALE = Locale('en');
