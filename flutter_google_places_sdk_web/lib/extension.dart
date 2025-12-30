10f3b91caaeda0bed323f38f3c0f74e91a212538
Matan Shukry <matanshukry@gmail.com>
2025-12-30 12:31:31 +0200

    docs, typo, fix path

--- flutter_google_places_sdk_web/lib/extension.dart
+++ flutter_google_places_sdk_web/lib/extension.dart
@@ -2,14 +2,16 @@ import 'dart:js_interop';
 
 import 'package:google_maps/google_maps_places.dart';
 
+/// Response type of [AutocompleteSuggestion.fetchAutocompleteSuggestions]
 @JS()
 extension type AutocompleteResponse._(JSObject _) implements JSObject {
-  // Access the 'suggestions' property as a JSArray
+  /// Access the 'suggestions' property as a JSArray
   external JSArray<AutocompleteSuggestion> get suggestions;
 }
 
+/// Response type of [Place.fetchFields]
 @JS()
 extension type FetchFieldsResponse._(JSObject _) implements JSObject {
-  // Access the 'place' property as a Place
+  /// Access the 'place' property as a Place
   external Place get place;
 }
